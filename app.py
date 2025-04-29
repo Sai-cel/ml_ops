a5624510ae5586215abef51322fa34f3394d238b
SAI_KUMAR_ALLA <2000avsaikumar@gmail.com>
2025-04-29 10:33:49 +0530

    final_chnages push in github

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-def function(a,b ,c , d):
+def function(a,b ,c , d , e):
     a = int(a)
     b= int(b)
     c= int(c)
-    d= int(d)
-    return a+c-b+d
+    d= float(d)
+    e=float(e)
+    return a+c-b+d-e
 
+print(function(3,5,7,8 , 10))
