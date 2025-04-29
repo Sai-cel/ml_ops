c489e1e04fadf043efeb811b6e7440834f822dab
SAI_KUMAR_ALLA <2000avsaikumar@gmail.com>
2025-04-29 10:35:44 +0530

    final

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-def function(a,b ,c , d , e):
+def function(a,b ,c , d , e , f ):
     a = int(a)
     b= int(b)
     c= int(c)
     d= float(d)
     e=float(e)
-    return a+c-b+d-e
+    f=int(f)
+    return a+c-b+d-e+f
 
-print(function(3,5,7,8 , 10))
+print(function(3,5,7,8 , 10 , 12))
